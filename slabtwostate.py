cb4038b4f146b9c54c46b4f3a8a49d356e2d0383
aKrylova <hohohox3@gmail.com>
2020-04-02 00:43:14 +0300

    add plot

--- slabtwostate.py
+++ slabtwostate.py
@@ -10,60 +10,63 @@
 # для графиков
 import numpy as np
 import random as rnd
-import matplotlib as mp
+import matplotlib.pyplot as plt
 
-#время
+# время
 T = 100
 # количество экспериментов
 N = 10000
-# количетсво экспериментов сколько раз были в заданном состоянии
-Nt = 0
 # матрица переходов
 m = np.array([
-        [0.3, 0.7],
-        [0.1, 0.9]
-    ])
+    [0.3, 0.7],
+    [0.1, 0.9]
+])
 
+
+def practice(n, t, startstate):
+    # количетсво экспериментов сколько раз были в заданном состоянии
+    Nt = 0
+    N = n
+    T = t
+    # начальное состояние
+    setState = startstate
+    # вероятности перехода из 0 в 0
+    p00 = m[0][0]
+    # из в 1 в 1
+    p11 = m[1][1]
+    # Nt/N оценка
+    result = 0
+    for n in range(0, N):
+        for t in range(0, T):
+            rnum = rnd.random()
+            # стартуем из 0
+            if setState:
+                # если выполнится, то переход
+                if rnum > p11:
+                    setState = 0
+            else:
+                if rnum > p00:
+                    setState = 1
+            # проверим если МЦ осталась в нужном состоянии
+        if setState == startState:
+            Nt += 1
+    return Nt / N
+
+arrayT = []
+arrayState0 = []
+arrayState1 = []
 startState = 0
-# начальное состояние
-setState = startState
-# вероятности перехода из 0 в 0
-p00 = m[0][0]
-# из в 1 в 1
-p11 = m[1][1]
-# Nt/N оценка
-result = 0
-
 for t in range(0, T) :
-    rnum = rnd.random()
-    # стартуем из 0
-    if setState :
-        # если выполнится, то переход
-        if rnum > p11 :
-            setState = 0
-    else :
-        if rnum > p00 :
-            setState = 1
-    # проверим если МЦ осталась в нужном состоянии
-    if setState == startState :
-        Nt += 1
-result = Nt / N
+    arrayT.append(t)
+    result = practice(N, T, 0)
+    print(result)
+    arrayState0.append(result)
+    result = practice(N, T, 1)
+    # result =
+    arrayState1.append(result)
+    print(result)
 
-
-
-#
-# rnum = rnd.random()
-# print(rnum)
-# for i in range(0, len(m)):
-#     for k in range(0, len(m)):
-#         if rnum > m[i][k]:
-#             state1 = rnum
-#             print(state1)
-
-
-
-
-
-
-
-
+plt.plot(arrayT, arrayState0)
+plt.plot(arrayT, arrayState1)
+plt.grid(True)
+plt.show()
